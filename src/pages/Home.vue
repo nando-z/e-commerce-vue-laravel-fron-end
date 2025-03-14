32d58f9eec3ffc3b3b95278d30d4d58d696d8fef
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-15 01:13:05 +0200

    Add Image Upload Form

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,28 @@
 <script setup>
+import { PhoneIcon } from "@heroicons/vue/24/outline";
+
 
 </script>
 
 <template>
     <DefaultLayout>
-        Hello
+        <div class="col-span-full">
+            <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
+            <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div class="text-center">
+                    <PhotoIcon class="mx-auto size-12 text-gray-300" aria-hidden="true" />
+                    <div class="mt-4 flex text-sm/6 text-gray-600">
+                        <label for="file-upload"
+                            class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500">
+                            <span>Upload a file</span>
+                            <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                        </label>
+                        <p class="pl-1">or drag and drop</p>
+                    </div>
+                    <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                </div>
+            </div>
+        </div>
+
     </DefaultLayout>
 </template>
