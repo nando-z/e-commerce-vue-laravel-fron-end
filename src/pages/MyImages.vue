8d28c9ad11331ecd3b14fcd1a2cb17fb375cbe09
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-15 01:52:57 +0200

    🌈 style: Update MyImages component to use dynamic image data and improve layout

--- src/pages/MyImages.vue
+++ src/pages/MyImages.vue
@@ -1,11 +1,11 @@
 <template>
 
     <DefaultLayout>
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-            <div v-for="image in 12" :key="image" class="relative">
-                <img :src="`https://picsum.photos/512/${image + 100}`" class="w-full h-full object-cover" alt="">
+        <div class="mb-6 mt-6  grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div v-for="image in images" :key="image.id" class="relative">
+                <img :src="image.url" class="w-full h-full object-cover" alt="">
                 <div class="absolute top-0 left-0 p-2 bg-black bg-opacity-50 text-white">
-                    Image {{ image }}
+                    {{ image.label }}
                 </div>
             </div>
         </div>
@@ -13,5 +13,10 @@
 </template>
 
 <script setup>
+
+const images = [
+    { id: 1, url: `https://picsum.photos/512/100`, label: "image 1" },
+    { id: 2, url: `https://picsum.photos/512/100`, label: "image 1" },
+]
 
 </script>
