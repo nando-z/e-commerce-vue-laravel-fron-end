f0debbae43e461be0ed3a79a3f19f99d0f574b04
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-15 01:48:34 +0200

    🌈 style: Update MyImages component layout to display images in a responsive grid

--- src/pages/MyImages.vue
+++ src/pages/MyImages.vue
@@ -1,7 +1,14 @@
 <template>
 
     <DefaultLayout>
-        <h1>My Images</h1>
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div v-for="image in 12" :key="image" class="relative">
+                <img :src="`https://picsum.photos/512/${image + 100}`" class="w-full h-full object-cover" alt="">
+                <div class="absolute top-0 left-0 p-2 bg-black bg-opacity-50 text-white">
+                    Image {{ image }}
+                </div>
+            </div>
+        </div>
     </DefaultLayout>
 </template>
 
