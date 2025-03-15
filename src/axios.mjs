c6622d919dc4812df68e05401ada9f1c77e3d13c
nando-z <157473087+nando-z@users.noreply.github.com>
2025-03-15 02:06:44 +0200

    fix: Add optional chaining for error response status in axios interceptor

--- src/axios.mjs
+++ src/axios.mjs
@@ -3,19 +3,16 @@ import router from "./router";
 
 export const axiosClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true,
-    withXSRFToken: true,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     },
 });
 
-
 axiosClient.interceptors.response.use(
     response => response,
     error => {
-        if (error.response.status === 401) {
+        if (error.response?.status === 401) {
             router.push({ name: "Login" });
         }
         return Promise.reject(error);
